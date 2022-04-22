bef6ffacc6c6d521f09527ac711808c286ae135c
Luiz Eduardo Prado Veltroni <eduardoveltroni@hotmail.com>
2022-04-22 15:28:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Template 
+
+Template para projetos Next.js utilizando Typescript
+
+--- 
+
+### Versões:
+
+- React **v17.0.2**
+- Next **v12.0.8**
+- TypeScript **v4.5.4**
+- Styled components **v5.3.3**
+- Eslint **v8.7.0**
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+// Libs
+import React from "react";
+import Head from "next/head";
+import type { NextPage } from "next";
+
+// Styles
+
+// Images
+
+// Components
+
+const Home: NextPage = () => {
+  return (
+    <Head>
+      <title>Título da página</title>
+    </Head>
+  );
+};
+
+export default Home;
