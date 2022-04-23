97f8cd7344275088437b89e6b2c8805c43d5d4c0
EduardooPV <eduardoveltroni@hotmail.com>
2022-04-23 17:20:04 -0300

    feat: cria componente title

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from './style'
+
+interface TitleProps {
+  children: string
+}
+
+export function Title({ children }: TitleProps) {
+  return (
+    <Container>
+      {children}
+    </Container>
+  )
+}
---
+++ src/components/Title/style.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+import c from "@styles/colors.json";
+import f from "@styles/typograph.json";
+
+export const Container = styled.h1`
+  width: fit-content;
+
+  margin: 0 auto;
+
+  font-family: Montserrat, sans-serif;
+  font-size: ${f.h3.fontSize};
+  line-height: ${f.h3.lineHeight};
+  color: ${c.neutral500};
+  text-align: center;
+
+  position: relative;
+
+  &::after,
+  ::before {
+    content: "";
+    display: block;
+    position: absolute;
+
+    height: 2px;
+    width: 50px;
+
+    background: ${c.primary300};
+  }
+
+  &::after {
+    left: -60px;
+    top: 50%;
+  }
+
+  &::before {
+    right: -60px;
+    top: 50%;
+  }
+
+  @media (min-width: 1023px) {
+    font-size: ${f.displaySmall.fontSize};
+    line-height: ${f.displaySmall.lineHeight};
+
+    &::after,
+    ::before {
+      width: 150px;
+    }
+
+    &::after {
+      left: -160px;
+      top: 50%;
+    }
+
+    &::before {
+      right: -160px;
+      top: 50%;
+    }
+  }
+`;
