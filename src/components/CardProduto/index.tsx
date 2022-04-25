6b39852742b2968dda8a17e8cba271bff6edb866
EduardooPV <eduardoveltroni@hotmail.com>
2022-04-25 19:11:23 -0300

    feat: cria componente product card

---
+++ src/components/CardProduto/index.tsx
@@ -0,0 +1,29 @@
+import { LinkItem } from "@components/Link";
+import Image from "next/image";
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { Container } from "./styles";
+
+interface CardProdutoProps {
+  props: {
+    img: string;
+    model: string;
+    link: string;
+  };
+}
+
+export function CardProduto({ props }: CardProdutoProps) {
+  return (
+    <Container>
+      <div>
+        <Image src={props.img} alt="Produt" layout="fill" />
+      </div>
+
+      <p>{props.model}</p>
+
+      <LinkItem href={props.link} transparent icon={<FaArrowRight />}>
+        VER MODELO
+      </LinkItem>
+    </Container>
+  );
+}
